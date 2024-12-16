c8d5b52b1ba5d3c20a8f5a456ebbf694a35c6b08
themmfuad <123576984+themmfuad@users.noreply.github.com>
2024-12-16 13:55:50 +0600

    progress on form validation

--- src/components/loginpage/LoginPageButtons.jsx
+++ src/components/loginpage/LoginPageButtons.jsx
@@ -1,18 +1,32 @@
 import './LoginPageButtons.css';
 
-export default function LoginPageButtons({buttonState, setButtonState}){
-    return(
-        <>
-        <div className="login-page-button-container">
-        <button className={buttonState == 'login' ? 'active' : ''} onClick={()=>{
-            setButtonState('login');
-        }}>Login</button>
+export default function LoginPageButtons({ buttonState, setButtonState }) {
+  return (
+    <>
+      <div className="login-page-button-container">
+        <button
+          className={buttonState == 'login' ? 'active' : ''}
+          onClick={() => {
+            buttonState == 'login'
+              ? console.log('submit')
+              : setButtonState('login');
+          }}
+        >
+          Login
+        </button>
         {console.log(buttonState)}
         <button>Demo</button>
-        <button className={buttonState == 'signup' ? 'active' : ''} onClick={()=>{
-            setButtonState('signup');
-        }}>Signup</button>
-        </div>
-        </>
-    );
+        <button
+          className={buttonState == 'signup' ? 'active' : ''}
+          onClick={() => {
+            buttonState == 'signup'
+              ? console.log('submit signup')
+              : setButtonState('signup');
+          }}
+        >
+          Signup
+        </button>
+      </div>
+    </>
+  );
 }
--- src/components/loginpage/LoginPageForm.jsx
+++ src/components/loginpage/LoginPageForm.jsx
@@ -1,23 +1,67 @@
-import './LoginPageForm.css'
+import { useState } from 'react';
+import './LoginPageForm.css';
 
-export default function LoginPageForm({buttonState}){
-    return (
-        <form id="login-form-container">
-            {(()=>{
-                if(buttonState=='signup'){
-                    return (
-                        <>
-                        <label htmlFor="nickname-input">Nickname:</label>
-                        <input type="text" name="nickname" id="nickname-input" />
-                        </>
-                    );
-                }
-            })()}
-            <label htmlFor="username-input">Username:</label>
-            <input type="text" name="username" id="username-input" />
+export default function LoginPageForm({ buttonState }) {
+  const [usernameInput, setUsernameInput] = useState({
+    currentValue: '',
+    isFocused: false,
+    isStarted: false,
+    isRuleFollower: false,
+    inputStates: {
+      breakMin: false,
+      breakMax: false,
+      noSign: true,
+    },
+  });
 
-            <label htmlFor="password-input">Password:</label>
-            <input type="text" name="password" id="password-input" />
-        </form>
-    );
+  return (
+    <form id="login-form-container">
+      {(() => {
+        if (buttonState == 'signup') {
+          return (
+            <>
+              <label htmlFor="nickname-input">Nickname:</label>
+              <input type="text" name="nickname" id="nickname-input" />
+            </>
+          );
+        }
+      })()}
+      <label htmlFor="username-input">Username:</label>
+      <input
+        maxLength={21}
+        type="text"
+        name="username"
+        id="username-input"
+        value={usernameInput.currentValue}
+        onFocus={() => setUsernameInput({ ...usernameInput, isFocused: true })}
+        onBlur={() => setUsernameInput({ ...usernameInput, isFocused: false })}
+        onChange={(event) => {
+          event.target.value.length >= 1
+            ? setUsernameInput({
+                ...usernameInput,
+                currentValue: event.target.value,
+                isStarted: true,
+              })
+            : setUsernameInput({
+                ...usernameInput,
+                currentValue: event.target.value,
+                isStarted: false,
+              });
+        }}
+      />
+      <ul
+        style={{
+          display: `${usernameInput.isFocused == true && usernameInput.isStarted ? 'block' : 'none'}`,
+        }}
+        id="username-input-rules"
+      >
+        <li>minimum 3 alpha-numeric character</li>
+        <li>no sign or symbols</li>
+        <li>not more than 21 character</li>
+      </ul>
+
+      <label htmlFor="password-input">Password:</label>
+      <input type="text" name="password" id="password-input" />
+    </form>
+  );
 }
